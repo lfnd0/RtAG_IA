cce0f66056387e00c66612e8fbb451f2b560d346
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-10-11 12:01:58 -0300

    Adição do projeto final PCV

---
+++ PCV/src/br/edu/pcv/ag/AlgoritmoGenetico.java
@@ -0,0 +1,91 @@
+package br.edu.pcv.ag;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+import br.edu.pcv.amostra.Populacao;
+import br.edu.pcv.rotas.Cidade;
+import br.edu.pcv.rotas.Rota;
+
+public class AlgoritmoGenetico {
+	public static final double TAXA_DE_MUTACAO = 0.25;
+	public static final int TAMANHO_DA_SELECAO = 3;
+	public static final int TAMANHO_DA_POPULACAO = 8;
+	public static final int NUMERO_DE_MELHOR_ROTA = 1;
+	public static final int NUMERO_DE_GERACOES = 30;
+
+	private ArrayList<Cidade> rotaInicial = null;
+
+	public AlgoritmoGenetico(ArrayList<Cidade> rotaInicial) {
+		this.rotaInicial = rotaInicial;
+	}
+
+	public ArrayList<Cidade> getRotaInicial() {
+		return rotaInicial;
+	}
+
+	public Populacao evoluiPopulacao(Populacao populacao) {
+		return inserirMutacaoPopulacao(crossoverPopulacao(populacao));
+	}
+
+	Populacao crossoverPopulacao(Populacao populacao) {
+		Populacao crossoverPopulacao = new Populacao(populacao.getRotas().size(), this);
+		IntStream.range(0, NUMERO_DE_MELHOR_ROTA)
+				.forEach(x -> crossoverPopulacao.getRotas().set(x, populacao.getRotas().get(x)));
+		IntStream.range(NUMERO_DE_MELHOR_ROTA, crossoverPopulacao.getRotas().size()).forEach(x -> {
+			Rota rota1 = selecionarPopulacao(populacao).getRotas().get(0);
+			Rota rota2 = selecionarPopulacao(populacao).getRotas().get(0);
+			crossoverPopulacao.getRotas().set(x, crossoverRota(rota1, rota2));
+		});
+		return crossoverPopulacao;
+	}
+
+	Populacao inserirMutacaoPopulacao(Populacao populacao) {
+		populacao.getRotas().stream().filter(x -> populacao.getRotas().indexOf(x) >= NUMERO_DE_MELHOR_ROTA)
+				.forEach(x -> inserirMutacaoRota(x));
+		return populacao;
+	}
+
+	Rota crossoverRota(Rota rota1, Rota rota2) {
+		Rota crossoverRota = new Rota(this);
+		Rota rotaTemp1 = rota1;
+		Rota rotaTemp2 = rota2;
+		if (Math.random() < 0.5) {
+			rotaTemp1 = rota2;
+			rotaTemp2 = rota1;
+		}
+		for (int i = 0; i < crossoverRota.getCidades().size() / 2; i++)
+			crossoverRota.getCidades().set(i, rotaTemp1.getCidades().get(i));
+		return removerNullCrossoverRota(crossoverRota, rotaTemp2);
+	}
+
+	private Rota removerNullCrossoverRota(Rota crossoverRota, Rota rota) {
+		rota.getCidades().stream().filter(x -> !crossoverRota.getCidades().contains(x)).forEach(CidadeX -> {
+			for (int i = 0; i < rota.getCidades().size(); i++) {
+				if (crossoverRota.getCidades().get(i) == null) {
+					crossoverRota.getCidades().set(i, CidadeX);
+					break;
+				}
+			}
+		});
+		return crossoverRota;
+	}
+
+	Rota inserirMutacaoRota(Rota rota) {
+		rota.getCidades().stream().filter(x -> Math.random() < TAXA_DE_MUTACAO).forEach(cidadeX -> {
+			int y = (int) (rota.getCidades().size() * Math.random());
+			Cidade cidadeY = rota.getCidades().get(y);
+			rota.getCidades().set(rota.getCidades().indexOf(cidadeX), cidadeY);
+			rota.getCidades().set(y, cidadeX);
+		});
+		return rota;
+	}
+
+	Populacao selecionarPopulacao(Populacao populacao) {
+		Populacao selecaoDaPopulacao = new Populacao(TAMANHO_DA_SELECAO, this);
+		IntStream.range(0, TAMANHO_DA_SELECAO).forEach(x -> selecaoDaPopulacao.getRotas().set(x,
+				populacao.getRotas().get((int) (Math.random() * populacao.getRotas().size()))));
+		selecaoDaPopulacao.sorteioDasRotasFuncaoFitness();
+		return selecaoDaPopulacao;
+	}
+}
---
+++ PCV/src/br/edu/pcv/main/Main.java
@@ -0,0 +1,69 @@
+package br.edu.pcv.main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.edu.pcv.ag.AlgoritmoGenetico;
+import br.edu.pcv.amostra.Populacao;
+import br.edu.pcv.rotas.Cidade;
+
+public class Main {
+
+	public ArrayList<Cidade> rotaInicial = new ArrayList<Cidade>(Arrays.asList(
+			new Cidade("Boston", 42.3601, -71.0589),
+			new Cidade("Houston", 29.7604, -95.3698),
+			new Cidade("Austin", 30.2672, -97.7431),
+			new Cidade("San Francisco", 37.7749, -122.4194),
+			new Cidade("Denver", 39.7392, -104.9903),
+			new Cidade("Los Angeles", 34.0522, -118.2437),
+			new Cidade("Chicago", 41.8781, -87.6298),
+			new Cidade("New York", 40.7128, -74.0059),
+			new Cidade("Dallas", 32.7767, -96.7970),
+			new Cidade("Seattle", 47.6062, -122.3321)));
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Populacao populacao = new Populacao(AlgoritmoGenetico.TAMANHO_DA_POPULACAO, main.rotaInicial);
+		populacao.sorteioDasRotasFuncaoFitness();
+		AlgoritmoGenetico algoritmoGenetico = new AlgoritmoGenetico(main.rotaInicial);
+		int numeroDaGeracao = 0;
+		main.printSaida(numeroDaGeracao++);
+		main.printPopulacao(populacao);
+		
+		while (numeroDaGeracao < AlgoritmoGenetico.NUMERO_DE_GERACOES) {
+			main.printSaida(numeroDaGeracao++);
+			populacao = algoritmoGenetico.evoluiPopulacao(populacao);
+			populacao.sorteioDasRotasFuncaoFitness();
+			main.printPopulacao(populacao);
+		}
+		
+		System.out.println("A melhor rota encontrada foi: " + populacao.getRotas().get(0) + " com uma distância de " + String.format("%.2f", populacao.getRotas().get(0).calcularDistanciaTotal()) + " quilômetros.");
+	}
+	
+	public void printPopulacao(Populacao populacao) {
+		populacao.getRotas().forEach(x -> {
+			System.out.println(Arrays.toString(x.getCidades().toArray()) + " | " + String.format("%.4f", x.getFuncaoFitness())
+			+ "  |  " + String.format("%.2f", x.calcularDistanciaTotal()));
+		});
+		System.out.println("");
+	}
+	
+	public void printSaida(int numeroDaGeracao) {
+		System.out.println("Geração #" +numeroDaGeracao);
+		String esquema1 = "Rota ";
+		String esquema2 = "Fitness | Distância (Km)";
+		int nomesDasCidades = 0;
+		for (int x = 0; x < rotaInicial.size(); x++) nomesDasCidades += rotaInicial.get(x).getNome().length();
+		int tamandoDoArray = nomesDasCidades + rotaInicial.size() * 2;
+		int comprimentoParcial = (tamandoDoArray - esquema1.length()) / 2;
+		for (int x = 0; x < comprimentoParcial; x++) System.out.print(" ");
+		System.out.print(esquema1);
+		for (int x = 0; x < comprimentoParcial; x++) System.out.print(" ");
+		if ((tamandoDoArray % 2) == 0) System.out.print(" ");
+		System.out.println(" | " + esquema2);
+		nomesDasCidades += esquema2.length() + 3;
+		for (int x = 0; x < nomesDasCidades + rotaInicial.size() * 2; x++)
+			System.out.print("-");
+		System.out.println("");
+	}
+}
