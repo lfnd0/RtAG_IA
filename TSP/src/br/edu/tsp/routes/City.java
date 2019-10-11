4a65bcbd082e53c2e49f867be839592aae3ce796
luizfernando <luiz-fsilva37@hotmail.com>
2019-10-11 00:39:29 -0300

    Adição do projeto TSP

---
+++ TSP/src/br/edu/tsp/routes/City.java
@@ -0,0 +1,41 @@
+package br.edu.tsp.routes;
+
+public class City {
+	private static final double EARTH_EQUATORIAL_RADIUS = 6378.1370D;
+	private static final double CONVERT_DEGREES_TO_RADIANS = Math.PI / 180D;
+	private static final double CONVERT_KM_TO_MILES = 0.621371;
+	
+	private String name;
+	private double latitude;
+	private double longitude;
+
+	public City(String name, double latitude, double longitude) {
+		this.name = name;
+		this.latitude = longitude * CONVERT_DEGREES_TO_RADIANS;
+		this.longitude = latitude* CONVERT_DEGREES_TO_RADIANS;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+	
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public double measureDistance(City city) {
+		double deltaLongitude = city.getLongitude() - this.getLongitude();
+		double deltaLatitude = city.getLatitude() - this.getLatitude();
+		double a = Math.pow(Math.sin(deltaLatitude / 2D), 2D) +
+				Math.cos(this.getLatitude()) * Math.cos(city.getLatitude()) * Math.pow(Math.sin(deltaLongitude / 2D), 2D);
+		return CONVERT_KM_TO_MILES * EARTH_EQUATORIAL_RADIUS * 2D * Math.atan2(Math.sqrt(a), Math.sqrt(1D - a));
+	}
+
+	public String toString() {
+		return getName();
+	}
+}
