5f272b8e25b6a02794fb9d379220beeb15b3ab69
luizfernando <luiz-fsilva37@hotmail.com>
2020-01-30 20:49:51 -0300

    Atualizações conexão com base de dados

--- PCV/src/br/edu/pcv/main/Main.java
+++ PCV/src/br/edu/pcv/main/Main.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 import br.edu.pcv.ag.AlgoritmoGenetico;
+import br.edu.pcv.bd.CriarBD;
 import br.edu.pcv.populacao.Populacao;
 import br.edu.pcv.rotas.Cidade;
 
 public class Main {
-
-	private static final String url = "jdbc:sqlite:pcv.db";
+	
 	static ArrayList<Cidade> rotaInicial;
 
 	public static void main(String[] args) throws SQLException {
@@ -37,11 +37,7 @@ public class Main {
 		}
 
 	}
-
-	public static String getUrl() {
-		return url;
-	}
-
+	
 	private static String entrada = JOptionPane.showInputDialog(null,
 			"Bem vindo(a) ao RtAG!" + "\n" + "Digite as cidades entre aspas simples e separadas por virgula.");
 
@@ -49,7 +45,7 @@ public class Main {
 
 	static ArrayList<Cidade> selecionarCidadeBD() throws SQLException {
 		ArrayList<Cidade> cidades = new ArrayList<Cidade>();
-		ResultSet resultSet = DriverManager.getConnection(getUrl()).createStatement()
+		ResultSet resultSet = DriverManager.getConnection(CriarBD.getUrl()).createStatement()
 				.executeQuery("select * from cidades " + sql);
 		while (resultSet.next()) {
 			cidades.add(new Cidade(resultSet.getString("nome"), Double.valueOf((resultSet.getString("latitude"))),
